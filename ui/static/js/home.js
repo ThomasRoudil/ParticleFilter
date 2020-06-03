b1512cdf4172bde0358c7539d287ab00dac815ac
Paul Ruelle <paul.ruelle@widmee.com>
2020-06-03 11:28:10 +0200

    enhance design, add compass compiling sass

--- ui/static/js/home.js
+++ ui/static/js/home.js
@@ -53,6 +53,24 @@ function drawMap(path) {
     });
 }
 
+function drawChart() {
+    var ctx = document.getElementById('chart').getContext('2d');
+
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Altitude profile',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+        options: {}
+    });
+}
+
 $(function () {
     $("img").mousedown(function(){
         return false;
@@ -68,6 +86,7 @@ $(function () {
 
             $('img').attr('src', "/get-dem/" + paths[0]);
             drawMap("/get-dem/" + paths[0]);
+            drawChart()
         }
     });
 
