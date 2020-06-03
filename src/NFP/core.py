3bce3df370e9dc5a61bba257a6a730a08eccb650
Toms <roudilt@gmail.com>
2020-06-03 02:31:31 +0200

    const height_maps en config

--- src/NFP/core.py
+++ src/NFP/core.py
@@ -2,6 +2,19 @@ import cv2
 import numpy as np
 import os
 from config import Config
+from scipy.ndimage.interpolation import map_coordinates
+
+TIME_STEPS = 50
+
+
+def _generate_trajectory(p1, p2):
+    speed = (p2[0] - p1[0], p2[1] - p1[1])
+    trajectory = [tuple(p1[index] + time * speed[index] / TIME_STEPS for index in range(2)) for time in range(TIME_STEPS + 1)]
+    return trajectory
+
+
+def _get_altitude_from_point(point, heightmap):
+    return round(map_coordinates(heightmap, list([element] for element in point), order=3)[0], 2)
 
 
 def generate_heightmaps():
@@ -15,14 +28,19 @@ def generate_heightmaps():
                 cv2.imwrite(os.path.join(Config.HEIGHTMAPS_PATH, filename), img)
 
 
-def generate_trajectory():
-    time_steps = 50
-    time = np.arange(1, time_steps)
-    v0 = 10
-    x = np.ones((time_steps, 2), dtype=float)
-    x[0, 0] = 10
-    x[0, 1] = 500
-    for t in time:
-        x[t, :] = x[t - 1, :] + [v0, 0]
-    return list(x)
+def generate_altitude_profile(positions, filename):
+    p1 = positions[0]
+    p2 = positions[1]
+    trajectory = _generate_trajectory(p1, p2)
+
+    dem_path = os.path.join(Config.DB_PATH, filename)
+    heightmap = np.loadtxt(dem_path)
+
+    altitude_profile = [_get_altitude_from_point(point, heightmap) for point in trajectory]
+    return altitude_profile
+
+
+if __name__ == '__main__':
+    positions = [(0, 100), (100, 950)]
+    generate_altitude_profile(positions, filename="BDALTIV2_75M_FXX_0375_6225_MNT_LAMB93_IGN69.asc")
 
--- src/run.py
+++ src/run.py
@@ -3,4 +3,4 @@ from NFP import core
 
 core.generate_heightmaps()
 
-app.run(host='127.0.0.1', port=3000, debug=True)
+app.run(host='127.0.0.1', port=3000, debug=True, threaded=True)
