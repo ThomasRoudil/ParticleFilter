f537152d6e9f704e315f270f94910d526950aef4
Paul Ruelle <paul.ruelle@widmee.com>
2020-06-09 13:33:03 +0200

    fix

--- src/run.py
+++ src/run.py
@@ -1,7 +1,2 @@
 from app import app
-from NFP import core
-
-core.generate_heightmaps_from_raw()
-core.generate_colormap_from_heightmaps()
-
 app.run(host='127.0.0.1', port=3000, debug=True, threaded=True)
