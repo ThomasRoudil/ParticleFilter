d2dd2bd479c26761f1d215142fd43357215355c3
Paul Ruelle <paul.ruelle@widmee.com>
2020-07-15 08:44:51 +0200

    fine tuning

--- api/app.py
+++ api/app.py
@@ -78,7 +78,7 @@ def compute_altitude_profile(args):
 def compute_particle_filter(args):
     altitude_profile = args['altitude_profile']
 
-    N = 200
+    N = 800
     particles = np.random.uniform(0, TIME_STEPS, N)
 
     tensor_particles = []
@@ -100,7 +100,7 @@ def compute_particle_filter(args):
 
         # Dynamics
         speed = 1
-        speed_noise = 1 * np.random.uniform(-1, 1, len(particles))
+        speed_noise = 0.5 * np.random.uniform(-1, 1, len(particles))
         particles = particles + speed_noise + speed
         particles = np.array([min(particle, 499) for particle in particles])
 
